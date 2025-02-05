b3dedbe5d31f95d4f80889e42d056ca505d18efc
SomewhatMay <umayeer.ahsan@hotmail.com>
2025-02-05 11:26:06 -0500

    outlined command structure

--- include/uvlib/command.hpp
+++ include/uvlib/command.hpp
@@ -1,6 +1,17 @@
 #pragma once
 
+namespace uvlib {
+
 class Command {
    private:
    public:
+    virtual void initialize();
+
+    virtual void execute();
+
+    virtual bool is_finished() = 0;
+
+    virtual void end(bool interrupted);
 };
+
+}  // namespace uvlib
---
+++ src/uvlib/command.cpp
@@ -0,0 +1,11 @@
+#include "uvlib/command.hpp"
+
+namespace uvlib {
+
+void Command::initialize() {}
+
+void Command::execute() {}
+
+void Command::end(bool interrupted) {}
+
+}  // namespace uvlib
