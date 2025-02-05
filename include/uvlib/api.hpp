b7f37eddca8a5a2e9c0cba16c1d68802fcc4f8b2
SomewhatMay <umayeer.ahsan@hotmail.com>
2025-02-05 11:06:51 -0500

    organized controller

---
+++ include/uvlib/api.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "main.h"
+
+namespace uvlib {
+class Controller {
+   protected:
+    pros::Controller controller;
+
+   public:
+    Controller(pros::controller_id_e_t id) : controller(id) {
+    }
+
+    pros::Controller get_controller() {
+        return controller;
+    }
+};
+}  // namespace uvlib
