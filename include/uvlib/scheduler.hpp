f10cad14fd952df70048f9dde5f6061bef2d6030
SomewhatMay <umayeer.ahsan@hotmail.com>
2025-02-06 20:00:56 -0500

    working on scheduler

--- include/uvlib/scheduler.hpp
+++ include/uvlib/scheduler.hpp
@@ -1,6 +1,18 @@
 #pragma once
 
+#include <forward_list>
+#include <stack>
+
+#include "uvlib/command.hpp"
+
+namespace uvlib {
 class Scheduler {
    private:
+    std::forward_list<std::stack<Command>> scheduled_commands;
+
    public:
+    void add_command(Command command);
+
+    std::forward_list<std::stack<Command>> get_scheduled_commands();
 };
+}  // namespace uvlib
