56e4939476b55846239ea9a656acff60cd2d8e38
May <40745122+SomewhatMay@users.noreply.github.com>
2025-02-07 00:27:31 -0500

    command header updated with new information

--- include/uvlib/command.hpp
+++ include/uvlib/command.hpp
@@ -1,11 +1,29 @@
 #pragma once
 
-namespace uvlib {
+#include <vector>
 
+#include "uvlib/scheduler.hpp"
+#include "uvlib/subsystem.hpp"
+
+namespace uvlib {
 class Command {
  private:
+  command_chain_t::iterator command_iterator;
+  std::list<Subsystem*> requirements;
+
+ protected:
+  void add_requirements(const std::vector<Subsystem>& requirements);
+
  public:
   Command();
+
+  void set_command_iterator(const command_chain_t::iterator& command_iterator);
+
+  const command_chain_t::iterator& get_command_iterator() const;
+
+  void cancel();
+
+  const std::list<Subsystem*>& get_requirements() const;
 
   virtual void initialize();
 
