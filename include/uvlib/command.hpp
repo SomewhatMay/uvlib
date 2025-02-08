403e786d071cea8a2e71e3337515e92bf7af5739
May <40745122+SomewhatMay@users.noreply.github.com>
2025-02-08 00:12:53 -0500

    documented Command::is_finished + prepared Command for scheduler update

--- include/uvlib/command.hpp
+++ include/uvlib/command.hpp
@@ -19,6 +19,23 @@ class Command {
    */
   int tick_number = -1;
 
+  /**
+   * Whether the command has been scheduled to be executed
+   * for the next tick or not.
+   *
+   * The value is automatically assigned as true by
+   * the scheduler when the command is scheduled
+   * or rescheduled.
+   *
+   * NOTE: If this value is true, the command is not
+   * guaranteed to run, rather, it is only guaranteed to
+   * considered by the scheduler. It may not run if multiple
+   * versions of this command is scheduled or this command
+   * requires another subsystem that has already been used
+   * by another command in the same tick.
+   */
+  bool alive = false;
+
  protected:
   void set_requirements(const std::initializer_list<Subsystem*>& requirements);
 
@@ -27,6 +44,15 @@ class Command {
 
   virtual void execute();
 
+  /**
+   * It is generally a good idea to keep all actions inside
+   * this method to be short and read-only operations. If
+   * the command is finished, it is strongly encouraged to
+   * move the finished logic to end().
+   *
+   * WARNING: This method should not assign any values inside
+   * any subsystems. Doing so can cause undefined behaviour.
+   */
   virtual bool is_finished() = 0;
 
   virtual void end(bool interrupted);
@@ -43,12 +69,21 @@ class Command {
 
   /* Getters and Setters */
 
+  /**
+   * Internal method.
+   */
   void set_command_iterator(const command_chain_t::iterator& command_iterator);
 
+  /**
+   * Internal method.
+   */
   const command_chain_t::iterator& get_command_iterator() const;
 
   const std::list<Subsystem*>& get_requirements() const;
 
+  /**
+   * Internal method.
+   */
   void set_tick_number(int tick_number);
 
   int get_tick_number() const;
