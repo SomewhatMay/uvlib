7886bec25e95f75669963906b3b14ac274bb448b
May <40745122+SomewhatMay@users.noreply.github.com>
2025-02-13 13:01:58 -0500

    joystick complete

---
+++ src/uvlib/input/joystick.cpp
@@ -0,0 +1,31 @@
+#include "uvlib/input/joystick.hpp"
+
+#include "main.h"
+#include "uvlib/typedefs.hpp"
+
+namespace uvl {
+explicit Joystick::Joystick(pros::Controller* controller, AnalogStick stick)
+    : controller(controller), stick(stick) {}
+
+int Joystick::get_x() const {
+  pros::controller_analog_e_t analog_e;
+
+  if (stick == AnalogStick::kLeft)
+    analog_e = pros::controller_analog_e_t::E_CONTROLLER_ANALOG_LEFT_X;
+  else
+    analog_e = pros::controller_analog_e_t::E_CONTROLLER_ANALOG_RIGHT_X;
+
+  return controller->get_analog(analog_e);
+}
+
+int Joystick::get_y() const {
+  pros::controller_analog_e_t analog_e;
+
+  if (stick == AnalogStick::kLeft)
+    analog_e = pros::controller_analog_e_t::E_CONTROLLER_ANALOG_LEFT_Y;
+  else
+    analog_e = pros::controller_analog_e_t::E_CONTROLLER_ANALOG_RIGHT_Y;
+
+  return controller->get_analog(analog_e);
+}
+}  // namespace uvl
